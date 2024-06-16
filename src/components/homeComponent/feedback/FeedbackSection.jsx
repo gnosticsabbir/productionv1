d3a91c79633248f8016063ccf75789db55c7031f
sabbir-cse-diu <sabbir50islam@gmail.com>
2024-06-17 05:52:45 +0600

    update

--- src/components/homeComponent/feedback/FeedbackSection.jsx
+++ src/components/homeComponent/feedback/FeedbackSection.jsx
@@ -1,28 +1,3 @@
-// import React from 'react'
-// import Feedback from '@/components/homeComponent/feedback/Feedback'
-
-// const FeedbackSection = () => {
-//   return (
-//     <div>
-//       <div className="max-w-[1200px] mx-auto px-4 py-20 pb-28 ">
-//         <div className="text-center mb-1">
-//             <h2 className="text-3xl p-1">Feedback</h2>
-//             <p className="text-xl">See what our client say's about us.</p>
-//         </div>
-//       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-//          <Feedback/>
-//          <Feedback/>
-//          <Feedback/>
-//       </div>
-//     </div>
-//     </div>
-//   )
-// }
-
-// export default FeedbackSection
-
-
-
 import React from 'react'
 import Feedback from '@/components/homeComponent/feedback/Feedback'
 
@@ -32,7 +7,7 @@ const FeedbackSection = () => {
       <div className="max-w-[1200px] mx-auto px-4 py-20 pb-28 ">
         <div className="text-center mb-1">
             <h2 className="text-3xl p-1">Feedback</h2>
-            <p className="text-xl">See what our client say&#39;s about us.</p>
+            <p className="text-xl">See what our client says about us.</p>
         </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
          <Feedback/>
@@ -45,4 +20,3 @@ const FeedbackSection = () => {
 }
 
 export default FeedbackSection
-
